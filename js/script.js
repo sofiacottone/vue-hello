9c006b21fd9ef8b7a327ac3f223c8f2c270f0f62
sofiacottone <cottonesofia@outlook.it>
2024-03-26 14:33:36 +0100

    feat: add dynamic image

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ createApp({
     data() {
         return {
             message: 'Hello Vue!',
+            // image: 'img/hello.jpg'
+            image: 'https://c4.wallpaperflare.com/wallpaper/835/299/811/simple-background-quote-minimalism-text-wallpaper-preview.jpg'
         };
     }
 }).mount('#app');
